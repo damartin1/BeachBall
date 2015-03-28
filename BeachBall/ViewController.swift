af05e251380d0452c6585361d606065738386cab
Robert Borkowski <rborkows@Roberts-MacBook-Pro.local>
2015-03-28 13:42:45 -0700

    Initial Commit

---
+++ BeachBall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeachBall
+//
+//  Created by Robert Borkowski on 3/28/15.
+//  Copyright (c) 2015 Robert Borkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
