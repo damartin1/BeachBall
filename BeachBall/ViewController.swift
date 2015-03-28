b7c786286078fa6037f9f10f28918eb68a8fcddc
Robert Borkowski <rborkows@Roberts-MacBook-Pro.local>
2015-03-28 13:57:56 -0700

    Initial setup. Added Fabric frameworks.

--- BeachBall/ViewController.swift
+++ BeachBall/ViewController.swift
@@ -7,11 +7,29 @@
 //
 
 import UIKit
+import TwitterKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let logInButton = TWTRLogInButton(logInCompletion: {
+            (session: TWTRSession!, error: NSError!) in
+            // play with Twitter session
+        })
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
+        
+        let authenticateButton = DGTAuthenticateButton(authenticationCompletion: {
+            (session: DGTSession!, error: NSError!) in
+            // play with Digits session
+        })
+        authenticateButton.center.y = self.view.center.y + CGFloat(80)
+        authenticateButton.center.x = self.view.center.x
+        self.view.addSubview(authenticateButton)
+
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
